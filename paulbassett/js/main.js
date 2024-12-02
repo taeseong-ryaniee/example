d21d67fe7b5b9560c76940da2e80267ba92bced3
TaeSeong Ryan YI <taeseong.culture@gmail.com>
2024-12-02 22:52:02 +0900

    ..

--- paulbassett/js/main.js
+++ paulbassett/js/main.js
@@ -1,22 +1,36 @@
 $(document).ready(function () {
-const swiper = new Swiper('.visual_swiper', { /* 팝업을 감싼는 요소의 class명 */
-
-	effect: "fade", /* fade 효과 */
-
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 100,
-		disableOnInteraction: true,
-	},
-
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-	},
-
+  $(".header .open_gnb").on("click", function () {
+    $(".header").addClass("mob");
+  });
+  $(".header .close_gnb").on("click", function () {
+    $(".header").removeClass("mob");
+  });
 });
-swiper.autoplay.stop();  /* 일시정지 기능 */
-swiper.autoplay.start();  /* 재생 기능 */
+
+$(document).ready(function () {
+  var swiper = new Swiper(".visual .visual_swiper", {
+    /* 팝업을 감싼는 요소의 class명 */ effect: "fade",
+    loop: true /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */,
+    /* fade 효과 */
+    autoplay: {
+      /* 팝업 자동 실행 */ delay: 100,
+    },
+
+    pagination: {
+      /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+      el: ".swiper-pagination" /* 해당 요소의 class명 */,
+      clickable: true /* 클릭하면 해당 팝업으로 이동할 것인지 값 */,
+    },
+  });
+});
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+document.addEventListener('aos:in', ({ detail }) => {
+  console.log('animated in', detail);
+});
+
+document.addEventListener('aos:out', ({ detail }) => {
+  console.log('animated out', detail);
 });
